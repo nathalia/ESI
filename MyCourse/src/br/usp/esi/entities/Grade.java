fc131a1e696040f93ac3eac6df0802007e40a59b
Nathalia Nunes <nathalialuznunes@gmail.com>
2011-09-28 20:54:35 -0300

    git ignore

--- MyCourse/src/br/usp/esi/entities/Grade.java
+++ MyCourse/src/br/usp/esi/entities/Grade.java
@@ -3,7 +3,16 @@ package br.usp.esi.entities;
 import java.util.List;
 
 public class Grade {
+	private String nomePeriodo;
 	private List<HorarioAula> horarios;
+	
+	public String getNomePeriodo() {
+		return nomePeriodo;
+	}
+
+	public void setNomePeriodo(String nomePeriodo) {
+		this.nomePeriodo = nomePeriodo;
+	}
 
 	public List<HorarioAula> getHorarios() {
 		return horarios;
