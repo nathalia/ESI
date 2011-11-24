b34a4ecf6a5b3c204dcbce5b5053f2743c43dede
Felipe Freire <felipe.freire@usp.br>
2011-11-24 00:35:24 -0200

    Paginas

--- MyCourseJSP/src/main/br/usp/esi/bean/ProfessorBean.java
+++ MyCourseJSP/src/main/br/usp/esi/bean/ProfessorBean.java
@@ -6,8 +6,6 @@ import java.util.List;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 
-import com.eds.common.jsf.BaseBackingBean;
-
 import main.br.usp.esi.entities.Professor;
 import main.br.usp.esi.service.ProfessorService;
 import main.br.usp.esi.service.impl.ProfessorServiceImpl;
@@ -18,11 +16,21 @@ public class ProfessorBean {
 
 	public List<Professor> getProfessores() {
 		List<Professor> professores = new ArrayList<Professor>();
-		System.out.println("SOCORRO");
 		ProfessorService professorService = new ProfessorServiceImpl();
 		for (Professor professor : professorService.listAll()) {
 			professores.add(professor);
 		}
 		return professores;
+	}
+	
+	public String editProfessor() {
+		return "professorPref";
+	}
+	
+	public List<Integer> getPrefs(){
+		List<Integer> prefs = new ArrayList<Integer>();
+		for(int i = 1; i< 37; i++)
+			prefs.add(i);
+		return prefs;
 	}
 }
