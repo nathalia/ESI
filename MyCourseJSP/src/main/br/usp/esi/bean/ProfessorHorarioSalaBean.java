35bd635cf35643521a2d216129ea0d367b2c7f44
Nathalia Nunes <nathalialuznunes@gmail.com>
2011-11-24 01:47:32 -0200

    view de ProfessorHorarioSala

--- MyCourseJSP/src/main/br/usp/esi/bean/ProfessorHorarioSalaBean.java
+++ MyCourseJSP/src/main/br/usp/esi/bean/ProfessorHorarioSalaBean.java
@@ -184,6 +184,6 @@ public class ProfessorHorarioSalaBean {
 		
 		schedule = scheduleImpl.schedule(professoresSelecionados, preferences, sala);
 		
-		return "teste";
+		return "escalonamentoResult";
 	}
 }
